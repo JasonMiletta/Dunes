6c9c8834248b4e105daa595733069482030adad1
Jason Miletta <jmiletta01@gmail.com>
2018-04-17 17:47:31 -0500

    Player Camera: Implement arrow keys for Camera input axis

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -11,10 +11,12 @@ public class PlayerCamera : MonoBehaviour {
 	#region PARAMETERS
 	public float smoothTime = 0.3f;
 	public Vector3 offsetVector = new Vector3(0f, -1.0f, 4.0f);
+	public const float cameraResetTime = 5.0f;
 	#endregion
 
 	#region STATE
 	private Vector3 velocity = Vector3.zero;
+	private float timeSinceLastCameraMovement = 0f;
 	#endregion
 	// Use this for initialization
 	void Start () {
@@ -23,9 +25,30 @@ public class PlayerCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Vector3 targetPosition = (trackedObject.transform.localPosition + offsetVector);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		if(timeSinceLastCameraMovement <= 0){
+			Vector3 targetPosition = (trackedObject.transform.position + offsetVector);
+			
+			transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		} else {
+			timeSinceLastCameraMovement -= Time.deltaTime;
+		}
 
 		transform.LookAt(trackedObject.transform, Vector3.up);
+		
+		lookUpdate();
+	}
+
+	void lookUpdate(){
+		float horizontal = Input.GetAxis("HorizontalCamera");
+		if(horizontal != 0) {
+			transform.RotateAround(trackedObject.transform.position, Vector3.up, horizontal);
+			timeSinceLastCameraMovement = cameraResetTime;
+		} else{
+			float mouseX = Input.GetAxis("Mouse X");
+			if(mouseX != 0){
+				transform.RotateAround(trackedObject.transform.position, Vector3.up, mouseX);
+				timeSinceLastCameraMovement = cameraResetTime;
+			}
+		}
 	}
 }
