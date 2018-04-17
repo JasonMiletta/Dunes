bc8f56e8ff396187df257ae98d16d1a06fe251a9
Jason Miletta <jmiletta01@gmail.com>
2018-04-17 17:20:53 -0500

    Day/Night Cycle: Implement simple day night directional light rotation

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+	#region COMPONENTS
+	public GameObject Sun;
+	#endregion
+
+	#region PARAMETERS
+	public float dayTimeSpeed = 1.0f;
+	#endregion
+
+	#region VARIABLES
+	private float tickAngle;
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		tickAngle = dayTimeSpeed / 360f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DayNightCycleUpdate();
+		
+	}
+
+	void DayNightCycleUpdate(){
+        this.transform.Rotate(new Vector3(0, 1), tickAngle);
+	}
+}
