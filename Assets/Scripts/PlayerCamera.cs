54512cc66076fae23e6e82f5100f82f76a64ac9a
Jason Miletta <jmiletta01@gmail.com>
2018-04-24 17:25:02 -0500

    Camera: Implement mouse/arrow horizontal rotation

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -10,14 +10,16 @@ public class PlayerCamera : MonoBehaviour {
 
 	#region PARAMETERS
 	public float smoothTime = 0.3f;
-	public Vector3 offsetVector = new Vector3(0f, 1.0f, -8.0f);
-	public const float cameraResetTime = 5.0f;
+	public Vector3 offsetVector = new Vector3(0f, 2.0f, -8.0f);
+	public const float cameraResetTime = 2.0f;
 	public float rotationSmoothTime = 5.0f;
+	public float lookSpeed = 100.0f;
 	#endregion
 
 	#region STATE
 	private Vector3 velocity = Vector3.zero;
 	private float timeSinceLastCameraMovement = 0f;
+	private float currentLookAngle = 0f;
 	private Vector3 targetPosition;
 	#endregion
 
@@ -38,50 +40,56 @@ public class PlayerCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//lookUpdate();
-		
-		//get rotation of trackedObject
-		Quaternion trackedObjectRotation = trackedObject.transform.rotation;
-		Vector3 trackedObjectEulerAngles = trackedObjectRotation.eulerAngles;
-
 		Vector3 targetPosition = trackedObject.transform.position;
 
 		//GET UP
 		targetPosition = trackedObject.transform.position + new Vector3(0.0f, offsetVector.y, 0.0f);
-		Debug.DrawLine(trackedObject.transform.position, targetPosition, Color.red);
 		
+	
+		//get rotation of trackedObject
+		Quaternion trackedObjectRotation = trackedObject.transform.rotation;
+		Vector3 trackedObjectEulerAngles = trackedObjectRotation.eulerAngles;
+
+		float rotationAngle = trackedObjectEulerAngles.y;
+		
+		lookUpdate();
+
+		if(timeSinceLastCameraMovement <= 0){
+			currentLookAngle = rotationAngle;
+		
+		} else {
+			rotationAngle = currentLookAngle;
+			timeSinceLastCameraMovement -= Time.deltaTime;
+		}
+	
 		//GET BEHIND
-		float yRotationInRadians = Mathf.Deg2Rad * (trackedObjectEulerAngles.y);
+		float yRotationInRadians = Mathf.Deg2Rad * (rotationAngle);
 		float xCoord = Mathf.Sin(yRotationInRadians);
 		float zCoord = Mathf.Cos(yRotationInRadians);
 		targetPosition = targetPosition + new Vector3(xCoord * offsetVector.z, 0.0f, zCoord * offsetVector.z);
-		Debug.DrawLine(trackedObject.transform.position, targetPosition, Color.red);
 
 		//Smooth rotation to look at target
 		Quaternion targetRotation = transform.rotation;
-		targetRotation.eulerAngles = new Vector3(targetRotation.x, trackedObjectEulerAngles.y, targetRotation.z); 
+		targetRotation.eulerAngles = new Vector3(targetRotation.x, rotationAngle, targetRotation.z); 
 		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSmoothTime);
 
 		//Move Camera to targetPosition, behind the trackedObject
 		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 		//transform.LookAt(trackedObject.transform, Vector3.up);
-		if(timeSinceLastCameraMovement <= 0){
-		} else {
-			timeSinceLastCameraMovement -= Time.deltaTime;
-		}
-
 		
 	}
 
 	void lookUpdate(){
 		float horizontal = Input.GetAxis("HorizontalCamera");
 		if(horizontal != 0) {
-			transform.RotateAround(trackedObject.transform.position, Vector3.up, horizontal);
+			//transform.RotateAround(trackedObject.transform.position, Vector3.up, horizontal);
+			currentLookAngle += horizontal * Time.deltaTime * lookSpeed;
 			timeSinceLastCameraMovement = cameraResetTime;
 		} else{
 			float mouseX = Input.GetAxis("Mouse X");
 			if(mouseX != 0){
-				transform.RotateAround(trackedObject.transform.position, Vector3.up, mouseX);
+				currentLookAngle += mouseX * Time.deltaTime * lookSpeed;
+				//transform.RotateAround(trackedObject.transform.position, Vector3.up, mouseX);
 				timeSinceLastCameraMovement = cameraResetTime;
 			}
 		}
